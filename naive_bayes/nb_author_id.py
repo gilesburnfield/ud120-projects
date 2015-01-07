c724508c32c5a61971d566b62e4bb4db6f682fc2
Giles Burnfield <gadburnfield@gmail.com>
2015-01-07 17:25:09 +0000

    nb first mini excercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,7 +13,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/Giles/Documents/Online learning/ML_intro_u/ud120-projects/tools")
 from email_preprocess import preprocess
 
 
@@ -21,6 +21,34 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time", round(time()-t1, 3), "s"
+
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
