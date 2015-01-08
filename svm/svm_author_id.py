d31c02d706145028cc0406e8b4f52d66d439c720
Giles Burnfield <gadburnfield@gmail.com>
2015-01-08 13:12:12 +0000

    SVM useing linear kernal

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/Giles/Documents/Online learning/ML_intro_u/ud120-projects/tools")
 from email_preprocess import preprocess
 
 
@@ -20,6 +20,25 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+print features_train[2:4]
+
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
 
 
